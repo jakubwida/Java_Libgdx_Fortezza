47df10808e8a8f85a35255631e73d3d7bcfc1862
Jakub Wida <jakub.wida@outlook.com>
2016-11-09 11:53:28 +0100

    firt sommit

---
+++ desktop/src/com/mygdx/Fortezza/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.Fortezza.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.Fortezza.Main_Fortezza_Class;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main_Fortezza_Class(), config);
+	}
+}
